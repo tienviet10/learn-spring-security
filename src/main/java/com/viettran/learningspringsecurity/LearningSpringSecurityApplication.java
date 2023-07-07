9bdda8e0c05eaa653b30f5cd552e5c9a337dafbf
vtran-at-speer <viet.tran@speer.io>
2023-07-07 19:08:34 -0400

    Initial commit.

---
+++ src/main/java/com/viettran/learningspringsecurity/LearningSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.viettran.learningspringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningSpringSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/viettran/learningspringsecurity/LearningSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viettran.learningspringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
